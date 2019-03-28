da41fa1cd29ad7beb2f5303d6531602c327ecde2
Ramez Alrawas <ramezrawas@gmail.com>
2019-03-28 09:01:27 +0100

    add xml2json.py
    This python code convert .xml files into .json files.

---
+++ xml2json.py
@@ -0,0 +1,25 @@
+import os
+from xmljson import badgerfish as bf
+from xml.etree.ElementTree import fromstring
+import json
+
+# open the XML file, convert to json and dump into a new file
+def xml_to_json(input, output):
+    i = 0
+    template_output = output
+    with open(input+ ".xml", "r") as input:
+        jsonOut = bf.data(fromstring(input.read()))
+        while(os.path.isfile(output+'.json')==True):
+            print("lastoutput"+output[-1])
+            print("i "+str(i))
+            if(output == template_output):
+                output+=str(i)
+            else:
+                output = template_output
+                output+=str(i)
+            i+=1
+
+    with open(output+ ".json","w+") as newFile:
+            json.dump(jsonOut, newFile, ensure_ascii=False)
+        
+xml_to_json(input('The .xml file: '), input('The .json file: '))
